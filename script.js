ccde5aaa2c0c62e269adb04a3c94a86daa4b829b
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-11-21 22:08:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const student = {
+    name: "Prasann",
+};
+
+Object.prototype.getkeys = function(){
+    return Object.keys(this);
+};
+
+const keys = student.getkeys();
+
+console.log(keys);
